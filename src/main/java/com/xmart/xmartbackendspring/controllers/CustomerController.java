df0443fbc7d47bddfe9a108d5af6ccc1c3ae334f
Nurdin Alawiyah <nurdinalawiyah@192.168.1.14>
2024-05-07 15:58:56 +0700

    add cors origin

--- src/main/java/com/xmart/xmartbackendspring/controllers/CustomerController.java
+++ src/main/java/com/xmart/xmartbackendspring/controllers/CustomerController.java
@@ -7,14 +7,12 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/customer")
+@CrossOrigin(origins = "*")
 public class CustomerController {
     CustomerService customerService;
 
